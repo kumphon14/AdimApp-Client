46669ec776b1e7e915338e213a042d0946c793e1
kumphon <kajohnponfff@gmail.com>
2024-12-02 15:47:31 +0700

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navber";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
